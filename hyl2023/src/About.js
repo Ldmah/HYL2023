ef6a07235e7448fe7ca028d1ccd6158f8aac20b8
Liam Mah <liam.d.mah@gmail.com>
2023-04-28 19:52:27 -0600

    Added base frontend stuff.

--- hyl2023/src/About.js
+++ hyl2023/src/About.js
@@ -1,12 +1,30 @@
-import Navbar from "./Navbar";
+import React from 'react';
+import Typed from 'typed.js';
 
 function About() {
-    return (
-        <div className="nfkldsajf">
-            nori
-        </div>
+    const el = React.useRef(null);
+  
+    React.useEffect(() => {
+        const typed = new Typed(el.current, {
+            strings: ["Hi, we are Team Nori Elite.", 'Welcome to our Hack Your Learning 2023 Project!', 'An informative waste tracker.'],
+            typeSpeed: 30,
+        });
         
-    )
+        return () => {
+            // Destroy Typed instance during cleanup to stop animation
+            typed.destroy();
+        };
+
+    }, []);
+    
+    return (
+        <>
+            <div id="about">
+                <h1><span ref={el} /></h1>
+            </div>
+        </>
+        
+    );
 }
 
 export default About;
--- hyl2023/src/App.js
+++ hyl2023/src/App.js
@@ -1,8 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import './App.css';
 import Navbar from './Navbar';
 import Survey from './Survey';
 import About from './About';
+
 
 
 function App() {
