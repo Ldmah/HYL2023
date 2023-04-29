3ca348605c8e6de06a60d0e59d28fb4cf80a0f75
ericmei123 <Eric.Mei@ucalgary.ca>
2023-04-28 19:28:57 -0600

    .

---
+++ hyl2023/src/Survey.js
@@ -0,0 +1,16 @@
+
+
+import React, { useState } from 'react';
+import Navbar from './Navbar';
+
+function Survey() {
+
+    return (
+        <div>
+            nori
+        </div>
+    )
+}
+
+
+export default Survey;
